3723836eee63836542065655a47ed9820315d810
vale1b <barberisvalentin85@gmail.com>
2024-05-29 16:02:48 -0300

    Subiendo clase con atributos estaticos y el objeto.

---
+++ Clases y Objetos.py
@@ -0,0 +1,11 @@
+# Esto es una clase y dentro de ella existen atributos estaticos.
+class Celular():
+    marca = 'Samsung'
+    modelo = 'S23'
+    camara = '48MP'
+
+# Esto es un objeto.    
+celular1 = Celular()
+
+# Aca podemos preguntar por las propiedades del celular.    
+print(celular1)
