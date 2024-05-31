0d9498e03ac22f575ce47a76666f40ac05863a64
vale1b <barberisvalentin85@gmail.com>
2024-05-31 17:47:13 -0300

    Herencia Multiple.

---
+++ Herencia Multiple.py
@@ -0,0 +1,29 @@
+class Persona:
+    def __init__(self, nombre, edad, nacionalidad):
+        self.nombre = nombre
+        self.edad = edad
+        self.nacionalidad = nacionalidad
+        
+    def hablar(self):
+        print('Hola, estoy hablando un poco.')
+        
+class Artista:
+    def __init__(self, habilidad):
+        self.habilidad = habilidad
+        
+    def mostrar_habilidad(self):
+        print(f'Mi habilidad es: {self.habilidad}')
+        
+class EmpleadoArtista(Persona, Artista):
+    def __init__(self, nombre, edad, nacionalidad, habilidad, empresa, salario):
+        Persona.__init__(self, nombre, edad, nacionalidad)
+        Artista.__init__(self, habilidad)
+        self.salario = salario
+        self.empresa = empresa
+        
+    def presentarse(self):
+        print(f'Hola, soy: {self.nombre}, tengo la habilidad de {super().mostrar_habilidad()} y trabajo en {self.empresa}')
+    
+roberto = EmpleadoArtista('Roberto', 43, 'argentino', 'Cantar', 'Google', 100000)
+
+roberto.presentarse()
